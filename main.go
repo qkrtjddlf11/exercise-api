8320b5ec23ec03758ebc1b2e79adf1a975c5d159
Park <qkrtjddlf11@naver.com>
2021-12-22 00:35:36 +0900

    Exercise-API v1.0.0-1

--- main.go
+++ main.go
@@ -20,29 +20,29 @@ const MariaDB string = "mysql"
 var db *sql.DB
 
 type Category struct {
-	Num         int     `json:"num"`
-	Name        string  `json:"name"`
-	Description *string `json:"description"`
-	Created_At  *string `json:"createdAt"`
-	Updated_At  *string `json:"updatedAt"`
-	Count       int     `json:"count"`
+	Id         int     `json:"id"`
+	Name       string  `json:"name"`
+	Desc       *string `json:"desc"`
+	Created_At *string `json:"createdAt"`
+	Updated_At *string `json:"updatedAt"`
+	Count      int     `json:"count"`
 	//Created_User string  `json:"created_user"`
 	//Updated_User *string `json:"updated_user"`
 }
 
 type Exercise struct {
-	Num         int     `json:"num"`
+	Id          int     `json:"id"`
 	Name        string  `json:"name"`
-	Description string  `json:"description"`
+	Desc        string  `json:"desc"`
 	Created_At  *string `json:"createdAt"`
 	Updated_At  *string `json:"updatedAt"`
 	Category_Id int     `json:"category_id"`
 }
 
 type ExerciseInCatetory struct {
-	Num         int    `json:"num"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Desc string `json:"desc"`
 }
 
 func errCheck(err error) error {
@@ -55,14 +55,14 @@ func errCheck(err error) error {
 
 func (c Category) categoryGetQueryAll() (categories []Category, err error) {
 	// select c.num, c.name, count(e.category_id) as \`count\` from category c left join exercise e on e.category_id = c.num group by c.num
-	rows, err := db.Query("SELECT c.num, c.name, c.description, c.createdAt, c.updatedAt, count(e.category_id) AS count FROM category c left join exercise e on e.category_id = c.num group by c.num")
+	rows, err := db.Query("SELECT c.id, c.name, c.`desc`, c.createdAt, c.updatedAt, count(e.category_id) AS count FROM category c left join exercise e on e.category_id = c.id group by c.id")
 	if err != nil {
 		return
 	}
 
 	for rows.Next() {
 		var category Category
-		rows.Scan(&category.Num, &category.Name, &category.Description, &category.Created_At, &category.Updated_At, &category.Count)
+		rows.Scan(&category.Id, &category.Name, &category.Desc, &category.Created_At, &category.Updated_At, &category.Count)
 		categories = append(categories, category)
 	}
 	defer rows.Close()
@@ -71,14 +71,14 @@ func (c Category) categoryGetQueryAll() (categories []Category, err error) {
 }
 
 func (e Exercise) exerciseGetQueryAll() (exercises []Exercise, err error) {
-	rows, err := db.Query("SELECT num, name, description, createdAt, updatedAt, category_id FROM exercise")
+	rows, err := db.Query("SELECT id, name, `desc`, createdAt, updatedAt, category_id FROM exercise")
 	if err != nil {
 		return
 	}
 
 	for rows.Next() {
 		var exercise Exercise
-		rows.Scan(&exercise.Num, &exercise.Name, &exercise.Description, &exercise.Created_At, &exercise.Updated_At, &exercise.Category_Id)
+		rows.Scan(&exercise.Id, &exercise.Name, &exercise.Desc, &exercise.Created_At, &exercise.Updated_At, &exercise.Category_Id)
 		exercises = append(exercises, exercise)
 	}
 	defer rows.Close()
@@ -87,14 +87,14 @@ func (e Exercise) exerciseGetQueryAll() (exercises []Exercise, err error) {
 }
 
 func (e ExerciseInCatetory) exerciseGetQueryInCategory(category_id int) (exercies []ExerciseInCatetory, err error) {
-	rows, err := db.Query("SELECT num, name, description FROM exercise WHERE category_id = ?", category_id)
+	rows, err := db.Query("SELECT id, name, `desc` FROM exercise WHERE category_id = ?", category_id)
 	if err != nil {
 		return
 	}
 
 	for rows.Next() {
 		var exercise ExerciseInCatetory
-		rows.Scan(&exercise.Num, &exercise.Name, &exercise.Description)
+		rows.Scan(&exercise.Id, &exercise.Name, &exercise.Desc)
 		exercies = append(exercies, exercise)
 	}
 	defer rows.Close()
@@ -102,53 +102,53 @@ func (e ExerciseInCatetory) exerciseGetQueryInCategory(category_id int) (exercie
 	return
 }
 
-func (c Category) categoryInsertQuery() (Num int, err error) {
+func (c Category) categoryInsertQuery() (Id int, err error) {
 	dateTime := time.Now().Format("2006-01-02 15:04:05")
-	stmt, err := db.Prepare("INSERT INTO category(name, description, createdAt) VALUES(?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO category(name, `desc`, createdAt) VALUES(?, ?, ?)")
 	if err != nil {
 		return
 	}
 
-	rs, err := stmt.Exec(c.Name, c.Description, dateTime)
+	rs, err := stmt.Exec(c.Name, c.Desc, dateTime)
 	if err != nil {
 		return
 	}
 
-	num, err := rs.LastInsertId()
+	id, err := rs.LastInsertId()
 	errCheck(err)
 
-	Num = int(num)
+	Id = int(id)
 	defer stmt.Close()
 
 	return
 }
 
-func (e Exercise) exerciseInsertQuery() (Num int, err error) {
+func (e Exercise) exerciseInsertQuery() (Id int, err error) {
 	dateTime := time.Now().Format("2006-01-02 15:04:05")
-	stmt, err := db.Prepare("INSERT INTO exercise(name, description, createdAt, category_id) VALUE(?, ?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO exercise(name, `desc`, createdAt, category_id) VALUE(?, ?, ?, ?)")
 	if err != nil {
 		return
 	}
 
-	rs, err := stmt.Exec(e.Name, e.Description, dateTime, e.Category_Id)
+	rs, err := stmt.Exec(e.Name, e.Desc, dateTime, e.Category_Id)
 	if err != nil {
 		return
 	}
 
-	num, err := rs.LastInsertId()
+	id, err := rs.LastInsertId()
 	errCheck(err)
 
-	Num = int(num)
+	Id = int(id)
 	defer stmt.Close()
 
 	return
 }
 
 func (c Category) categoryDeleteQuery() (rows int, err error) {
-	stmt, err := db.Prepare("DELETE FROM category WHERE num = ?")
+	stmt, err := db.Prepare("DELETE FROM category WHERE id = ?")
 	errCheck(err)
 
-	rs, err := stmt.Exec(c.Num)
+	rs, err := stmt.Exec(c.Id)
 	errCheck(err)
 
 	row, err := rs.RowsAffected()
@@ -160,10 +160,10 @@ func (c Category) categoryDeleteQuery() (rows int, err error) {
 }
 
 func (e Exercise) exerciseDeleteQuery() (rows int, err error) {
-	stmt, err := db.Prepare("DELETE FROM exercise WHERE num = ?")
+	stmt, err := db.Prepare("DELETE FROM exercise WHERE id = ?")
 	errCheck(err)
 
-	rs, err := stmt.Exec(e.Num)
+	rs, err := stmt.Exec(e.Id)
 	errCheck(err)
 
 	row, err := rs.RowsAffected()
@@ -176,9 +176,9 @@ func (e Exercise) exerciseDeleteQuery() (rows int, err error) {
 func (c Category) categoryUpdateQuery() (rows int, err error) {
 	dateTime := time.Now().Format("2006-01-02 15:04:05")
 	if len(c.Name) == 0 || c.Name == "" {
-		stmt, err := db.Prepare("UPDATE category SET description = ?, updatedAt = ? WHERE num = ?")
+		stmt, err := db.Prepare("UPDATE category SET `desc` = ?, updatedAt = ? WHERE id = ?")
 		errCheck(err)
-		rs, err := stmt.Exec(c.Description, dateTime, c.Num)
+		rs, err := stmt.Exec(c.Desc, dateTime, c.Id)
 		errCheck(err)
 
 		row, err := rs.RowsAffected()
@@ -186,9 +186,9 @@ func (c Category) categoryUpdateQuery() (rows int, err error) {
 		defer stmt.Close()
 		rows = int(row)
 	} else {
-		stmt, err := db.Prepare("UPDATE category SET name = ?, description = ?, updatedAt = ? WHERE num = ?")
+		stmt, err := db.Prepare("UPDATE category SET name = ?, `desc` = ?, updatedAt = ? WHERE id = ?")
 		errCheck(err)
-		rs, err := stmt.Exec(c.Name, c.Description, dateTime, c.Num)
+		rs, err := stmt.Exec(c.Name, c.Desc, dateTime, c.Id)
 		errCheck(err)
 
 		row, err := rs.RowsAffected()
@@ -203,10 +203,10 @@ func (c Category) categoryUpdateQuery() (rows int, err error) {
 func (e Exercise) exerciseUpdateQuery() (rows int, err error) {
 	dateTime := time.Now().Format("2006-01-02 15:04:05")
 	if len(e.Name) == 0 || e.Name == "" {
-		stmt, err := db.Prepare("UPDATE exercise SET description = ?, updatedAt = ? WHERE num = ?")
+		stmt, err := db.Prepare("UPDATE exercise SET `desc` = ?, updatedAt = ? WHERE id = ?")
 		errCheck(err)
 
-		rs, err := stmt.Exec(e.Name, dateTime, e.Num)
+		rs, err := stmt.Exec(e.Name, dateTime, e.Id)
 		errCheck(err)
 
 		row, err := rs.RowsAffected()
@@ -214,10 +214,10 @@ func (e Exercise) exerciseUpdateQuery() (rows int, err error) {
 		defer stmt.Close()
 		rows = int(row)
 	} else {
-		stmt, err := db.Prepare("UPDATE exercise SET name = ?, description = ?, updatedAt = ? WHERE num = ?")
+		stmt, err := db.Prepare("UPDATE exercise SET name = ?, `desc` = ?, updatedAt = ? WHERE id = ?")
 		errCheck(err)
 
-		rs, err := stmt.Exec(e.Name, e.Description, dateTime, e.Num)
+		rs, err := stmt.Exec(e.Name, e.Desc, dateTime, e.Id)
 		errCheck(err)
 
 		row, err := rs.RowsAffected()
@@ -238,10 +238,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println("env [DBNAME]:", os.Getenv("DBNAME"), os.Getenv("DBUSER"))
-
 	// Connection DB
-	dbString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DBUSER"), os.Getenv("DBPASSWD"), os.Getenv("DBIPADDR"), os.Getenv("DBPORT"), os.Getenv("DBNAME"))
+	dbString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DBUSER"),
+		os.Getenv("DBPASSWD"),
+		os.Getenv("DBIPADDR"),
+		os.Getenv("DBPORT"),
+		os.Getenv("DBNAME"))
 	db, err = sql.Open(MariaDB, dbString)
 	errCheck(err)
 	defer db.Close()
@@ -278,50 +281,50 @@ func main() {
 	})
 
 	// Create Specific Category.
-	// curl http://127.0.0.1:8080/api/category/create -X POST -d '{"name": "등", "description": "등 근육의 전반적인 향상", "created_user": "Park"}' -H "Content-Type: application/json"
+	// curl http://127.0.0.1:8080/api/category/create -X POST -d '{"name": "등", "desc": "등 근육의 전반적인 향상", "created_user": "Park"}' -H "Content-Type: application/json"
 	router.POST("/api/category", func(c *gin.Context) {
 		category := Category{}
 		err := c.Bind(&category)
 		errCheck(err)
 
-		Num, err := category.categoryInsertQuery()
+		Id, err := category.categoryInsertQuery()
 		errCheck(err)
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf(" %d Successfully Created", Num),
+			"message": fmt.Sprintf(" %d Successfully Created", Id),
 		})
 	})
 
 	// Delete Specific Category.
 	// curl http://127.0.0.1:8080/api/category/delete/5 -X DELETE
-	router.DELETE("/api/category/:num", func(c *gin.Context) {
-		num := c.Param("num")
+	router.DELETE("/api/category/:category_id", func(c *gin.Context) {
+		id := c.Param("id")
 
-		Num, err := strconv.ParseInt(num, 10, 10)
+		Id, err := strconv.ParseInt(id, 10, 10)
 		errCheck(err)
 
-		category := Category{Num: int(Num)}
+		category := Category{Id: int(Id)}
 		rows, err := category.categoryDeleteQuery()
 		if err != nil {
 			log.Fatal(err, rows)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Successfully deleted num: %d", Num),
+			"message": fmt.Sprintf("Successfully deleted category_id: %d", Id),
 		})
 
 	})
 
 	// Update Specific Category.
-	// curl http://127.0.0.1:8080/api/category/patch/16 -X PATCH -d {"name": "변경된 카테고리", "description": "Blah Blah.."}
-	router.PATCH("/api/category/:num", func(c *gin.Context) {
-		num := c.Param("num")
+	// curl http://127.0.0.1:8080/api/category/patch/16 -X PATCH -d {"name": "변경된 카테고리", "desc": "Blah Blah.."}
+	router.PATCH("/api/category/:category_id", func(c *gin.Context) {
+		id := c.Param("category_id")
 
-		Num, err := strconv.Atoi(num)
+		Id, err := strconv.Atoi(id)
 		errCheck(err)
 
 		category := Category{}
-		category.Num = Num
+		category.Id = Id
 		err = c.Bind(&category)
 		errCheck(err)
 
@@ -331,7 +334,7 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Successfully update num: %d", Num),
+			"message": fmt.Sprintf("Successfully update category_id: %d", Id),
 		})
 	})
 
@@ -346,7 +349,7 @@ func main() {
 	})
 
 	// Create Specific Exercise.
-	// curl http://127.0.0.1:8080/api/exercise/create/2 -X POST -d '{"name": "벤치 프레스", "description": "가슴 근육 향상"}' -H "Content-Type: application/json"
+	// curl http://127.0.0.1:8080/api/exercise/create/2 -X POST -d '{"name": "벤치 프레스", "desc": "가슴 근육 향상"}' -H "Content-Type: application/json"
 	router.POST("/api/exercise/:category_id", func(c *gin.Context) {
 		category_id := c.Param("category_id")
 		exercise := Exercise{}
@@ -354,44 +357,44 @@ func main() {
 		errCheck(err)
 
 		exercise.Category_Id, _ = strconv.Atoi(category_id)
-		Num, err := exercise.exerciseInsertQuery()
+		Id, err := exercise.exerciseInsertQuery()
 		errCheck(err)
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf(" %d Successfully Created", Num),
+			"message": fmt.Sprintf(" %d Successfully Created", Id),
 		})
 	})
 
 	// Delete Specific Exercise.
 	// curl http://127.0.0.1:8080/api/exercise/delete/5 -X DELETE
-	router.DELETE("/api/exercise/:num", func(c *gin.Context) {
-		num := c.Param("num")
+	router.DELETE("/api/exercise/:exercise_id", func(c *gin.Context) {
+		id := c.Param("exercise_id")
 
-		Num, err := strconv.ParseInt(num, 10, 10)
+		Id, err := strconv.ParseInt(id, 10, 10)
 		errCheck(err)
 
-		exercise := Exercise{Num: int(Num)}
+		exercise := Exercise{Id: int(Id)}
 		rows, err := exercise.exerciseDeleteQuery()
 		if err != nil {
 			log.Fatal(err, rows)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Successfully deleted num: %d", Num),
+			"message": fmt.Sprintf("Successfully deleted exercise_id: %d", Id),
 		})
 
 	})
 
 	// Update Specific Exercise.
-	// curl http://127.0.0.1:8080/api/exercise/patch/16 -X PATCH -d {"name": "변경된 카테고리", "description": "Blah Blah.."}
-	router.PATCH("/api/exercise/:num", func(c *gin.Context) {
-		num := c.Param("num")
+	// curl http://127.0.0.1:8080/api/exercise/patch/16 -X PATCH -d {"name": "변경된 카테고리", "desc": "Blah Blah.."}
+	router.PATCH("/api/exercise/:exercise_id", func(c *gin.Context) {
+		id := c.Param("exercise_id")
 
-		Num, err := strconv.Atoi(num)
+		Id, err := strconv.Atoi(id)
 		errCheck(err)
 
 		exercise := Exercise{}
-		exercise.Num = Num
+		exercise.Id = Id
 		err = c.Bind(&exercise)
 		errCheck(err)
 
@@ -401,7 +404,7 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Successfully update num: %d", Num),
+			"message": fmt.Sprintf("Successfully update exercise_id: %d", Id),
 		})
 	})
 
